4fb42ace744a8bf0aba039b4db7b834560417e12
Tiago Cordeiro <tiago@mulhergorila.com>
2018-03-09 14:52:14 -0300

    Adicionado função que cria arquivo compactado `.zip`

--- wpchildmaker/wpchildmaker.py
+++ wpchildmaker/wpchildmaker.py
@@ -1,3 +1,4 @@
+from zipfile import ZipFile
 import jinja2
 import click
 import os
@@ -25,12 +26,43 @@ def make_child(theme_name, customer_name, customer_site):
 
     f_footer = open('mychilds/' + theme_name + '/' + 'footer.php', 'w')
     f_footer.write(template_footer.render(customer_name=customer_name))
+    f_footer.close()
 
     f_functions = open('mychilds/' + theme_name + '/' + 'functions.php', 'w')
     f_functions.write(template_functions.render(customer_site=customer_site))
+    f_functions.close()
 
     f_style = open('mychilds/' + theme_name + '/' + 'style.css', 'w')
     f_style.write(template_style.render(theme_name=theme_name))
+    f_style.close()
+
+    ziparchive(theme_name)
+
+
+def ziparchive(theme_name):
+    # initializing empty file paths list
+    file_paths = []
+    os.chdir('./mychilds/')
+
+    # crawling through directory and subdirectories
+    for root, directories, files in os.walk('./' + theme_name + '/'):
+        for filename in files:
+            # join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+
+    # printing the list of all files to be zipped
+    print('Following files will be zipped:')
+    for file_name in file_paths:
+        print(file_name)
+
+    # writing files to a zipfile
+    with ZipFile('./' + theme_name + '.zip', 'w') as zip:
+        # writing each file one by one
+        for file in file_paths:
+            zip.write(file)
+
+    print('All files zipped successfully!')
 
 
 if __name__ == '__main__':
