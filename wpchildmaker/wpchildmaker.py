32f0bcdafbb6bb72ce257c9340d067859fd61aab
Tiago Cordeiro <tiago@mulhergorila.com>
2019-04-23 12:56:28 -0300

    Refatora.
    Melhora nome das variáveis e opções.

--- wpchildmaker/wpchildmaker.py
+++ wpchildmaker/wpchildmaker.py
@@ -9,21 +9,21 @@ from halo import Halo
 
 
 @click.command()
-@click.option("--theme_name", prompt="Child theme name", help="Não use espaço ou caracteres especiais")
+@click.option("--child_name", prompt="Child theme name", help="Não use espaço ou caracteres especiais")
 @click.option("--customer_name", prompt="Customer name", help="Nome do cliente")
 @click.option("--customer_site", prompt="url do site", help="ex. https://www.mulhergorila.com")
-@click.option("--template", prompt="Template name", help="ex. Divi", default="Divi")
-def make_child(theme_name, customer_name, customer_site, template):
+@click.option("--template_name", prompt="Template name", help="ex. Divi", default="Divi")
+def make_child(child_name, customer_name, customer_site, template_name):
     spinner = Halo(text="Loading", spinner="dots")
     spinner.start()
 
     pathname = os.path.dirname(sys.argv[0])
-    template_dir = os.path.abspath(pathname) + "/templates/" + template
+    template_dir = os.path.abspath(pathname) + "/templates/" + template_name
     template_files = os.listdir(template_dir)
     env = jinja2.Environment()
     env.loader = jinja2.FileSystemLoader(template_dir)
 
-    child_path = os.path.abspath(pathname) + "/mychilds/" + theme_name + "/"
+    child_path = os.path.abspath(pathname) + "/mychilds/" + child_name + "/"
     directory = os.path.dirname(child_path)
 
     if not os.path.exists(directory):
@@ -35,14 +35,14 @@ def make_child(theme_name, customer_name, customer_site, template):
             spinner.succeed("Arquivo " + file + " Copiado")
         else:
             template_file = env.get_template(file)
-            template_writer = open("mychilds/" + theme_name + "/" + file, "w")
-            template_writer.write(template_file.render(theme_name=theme_name,
+            template_writer = open("mychilds/" + child_name + "/" + file, "w")
+            template_writer.write(template_file.render(theme_name=child_name,
                                                        customer_name=customer_name,
                                                        customer_site=customer_site, ))
             spinner.succeed("Arquivo " + file + " Gerado")
 
     spinner.succeed("Arquivos gerados.")
-    ziparchive(theme_name)
+    ziparchive(child_name)
     spinner.stop()
 
 
